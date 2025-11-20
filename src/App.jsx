712808d4c43922f6879424bd1bdd4294c2d09cc2
IvanIvanovJS <sibsmartco@gmail.com>
2025-11-20 20:14:41 +0200

    feat(Header) Add Header Component, and Header.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/layout/header/Header"
 
 function App() {
 
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import styles from './Header.module.css';
+import GhostIcon from '../../UI/ghostIcon/GhostIcon'
+import { Link } from 'react-router';
+
+
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        // Header Container with Nav as Sibling
+        <div className={styles.headerWrapper}>
+            <header className={styles.header}>
+                {/* Logo Section */}
+                <div className={styles.logo}>
+                    <GhostIcon />
+                    <span className={styles.siteName}>GhostMiner</span>
+                </div>
+
+                {/* Desktop Navigation */}
+                <nav className={styles.navDesktop}>
+                    <Link to="/" className={styles.navLink} >Home</Link>
+                    <Link to="/templates" className={styles.navLink} >Templates</Link>
+                    <Link to="/my-templates" className={styles.navLink}>My Templates</Link>
+                    <Link to="/login" className={styles.navLink}>Login</Link>
+                </nav>
+
+                {/* Mobile Menu Icon */}
+                <div className={`${styles.menuIcon} ${isMenuOpen ? styles.menuIconOpen : ''}`} onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </header>
+
+            {/* Mobile Navigation as Sibling */}
+            <nav className={`${styles.navMobile} ${isMenuOpen ? styles.navMobileOpen : ''}`}>
+                <Link to="/" className={styles.navLink}>Home</Link>
+                <Link to="/templates" className={styles.navLink}>Templates</Link>
+                <Link to="/my-templates" className={styles.navLink}>My Templates</Link>
+                <Link to="/login" className={styles.navLink}>Login</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
