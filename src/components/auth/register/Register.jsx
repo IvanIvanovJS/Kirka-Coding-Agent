ffcec425bbda63fe134b977ceb096a58e7cd78d8
IvanIvanovJS <sibsmartco@gmail.com>
2025-11-30 12:59:01 +0200

    created(UseForm) hook, implemented Register functionality

--- src/components/auth/register/Register.jsx
+++ src/components/auth/register/Register.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router";
+import useFetch from "../../../hooks/useFetch";
+import useForm from "../../../hooks/useForm";
 import styles from "./Register.module.css";
 
+const initialValues = {
+	email: "",
+	password: "",
+	confirmPassword: "",
+};
+
 export default function Register() {
+	const [user, setUser] = useState(null);
+
+	const { values, onChangeHandler, formAction } = useForm(
+		registerHandler,
+		initialValues,
+	);
+	const {
+		data: userData,
+		isLoading,
+		error,
+	} = useFetch("http://localhost:3030/users/register", {}, "POST", {}, user);
+
+	function registerHandler(values) {
+		const { email, password, confirmPassword } = values;
+
+		if (!email) {
+			throw new Error("Email is required!");
+		}
+		if (!password) {
+			throw new Error("Password is required!");
+		}
+
+		if (password !== confirmPassword) {
+			throw new Error("Passwords does not match!");
+		}
+
+		setUser({ email, password });
+	}
+	console.log(userData);
 	return (
 		<div className={styles.registerContainer}>
-			<form className={styles.registerForm}>
+			<form className={styles.registerForm} action={formAction}>
 				<h2 className={styles.formTitle}>Register</h2>
 
 				<div className={styles.inputGroup}>
@@ -15,6 +53,8 @@ export default function Register() {
 						type="email"
 						id="email"
 						name="email"
+						value={values.email}
+						onChange={onChangeHandler}
 						className={styles.input}
 						placeholder="Enter your email"
 					/>
@@ -28,6 +68,8 @@ export default function Register() {
 						type="password"
 						id="password"
 						name="password"
+						value={values.password}
+						onChange={onChangeHandler}
 						className={styles.input}
 						placeholder="Enter your password"
 					/>
@@ -41,6 +83,8 @@ export default function Register() {
 						type="password"
 						id="confirmPassword"
 						name="confirmPassword"
+						value={values.confirmPassword}
+						onChange={onChangeHandler}
 						className={styles.input}
 						placeholder="Confirm your password"
 					/>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,22 @@
-export default function useForm() {}
+import { useState } from "react";
+
+export default function useForm(callback, initialValues) {
+	const [values, setValues] = useState(initialValues);
+
+	const onChangeHandler = (e) => {
+		setValues((state) => ({
+			...state,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const formAction = async (formData) => {
+		await callback(values, formData);
+	};
+
+	return {
+		values,
+		onChangeHandler,
+		formAction,
+	};
+}
