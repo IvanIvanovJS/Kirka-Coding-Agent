9c76eec71385183b1fbe43eea6339a2a63b395f8
IvanIvanovJS <sibsmartco@gmail.com>
2025-12-04 21:54:23 +0200

    feat(PreviewPanel) Dynamic expand/shrink preview panel

--- src/components/agentApp/previewPanel/PreviewPanel.jsx
+++ src/components/agentApp/previewPanel/PreviewPanel.jsx
@@ -1,10 +1,14 @@
+import { useAgentApp } from '../../../contexts';
 import PreviewFrame from './previewFrame/PreviewFrame';
 import styles from './PreviewPanel.module.css';
 import PreviewToolbar from './previewToolbar/PreviewToolbar';
 
 export default function PreviewPanel() {
+	const { isSidebarVisible } = useAgentApp();
 	return (
-		<div className={styles.previewPanel}>
+		<div
+			className={`${styles.previewPanel} ${!isSidebarVisible ? styles.previewPanelExpanded : ''}`}
+		>
 			<PreviewToolbar />
 			<PreviewFrame />
 		</div>
