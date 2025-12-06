b1ccf6abae6184370d228ec44bc6a29dafc65f2a
IvanIvanovJS <sibsmartco@gmail.com>
2025-12-06 22:42:50 +0200

    ref(DeleteComment) Moved component in an other folder

---
+++ src/components/templateDepend/templateDetails/commentsSection/deleteComment/DeleteComment.jsx
@@ -0,0 +1,56 @@
+import { useUser } from '../../../../../contexts';
+import useFetch from '../../../../../hooks/useFetch';
+import styles from './DeleteComment.module.css';
+import DeleteConfirmationModal from './DeleteConfirmationModal';
+
+export default function DeleteComment({ comment, updateCommentHandler }) {
+	const { user } = useUser();
+
+	const { error, refetch } = useFetch(
+		`http://localhost:3030/data/comments/${comment._id}`,
+		null,
+		'DELETE',
+		null,
+		null,
+		false,
+	);
+
+	const deleteCommentHandler = async () => {
+		await refetch(null, {
+			'X-Authorization': user?.accessToken,
+		});
+		updateCommentHandler({ type: 'delete', payload: comment._id });
+	};
+
+	if (error) {
+		//TODO add global error handling
+	}
+
+	return (
+		<>
+			<button
+				type="button"
+				className={styles.deleteButton}
+				title="Delete comment"
+				onClick={() => deleteCommentHandler()}
+			>
+				<svg
+					width="18"
+					height="18"
+					viewBox="0 0 24 24"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M3 6h18M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M10 11v6M14 11v6"
+						stroke="currentColor"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+				</svg>
+			</button>
+			<DeleteConfirmationModal />
+		</>
+	);
+}
