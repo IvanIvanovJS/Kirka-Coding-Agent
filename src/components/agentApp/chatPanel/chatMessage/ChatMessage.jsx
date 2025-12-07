7bdd4ce1b49b278914f68b04b513b1d9f731590e
IvanIvanovJS <sibsmartco@gmail.com>
2025-12-07 19:15:51 +0200

    create(ChatMessage) Add component handling messages with ai and css module

---
+++ src/components/agentApp/chatPanel/chatMessage/ChatMessage.jsx
@@ -0,0 +1,24 @@
+import GhostIcon from '../../../UI/ghostIcon/GhostIcon';
+import formatEpoch from '../../../../utils/epochConverter';
+import styles from './ChatMessage.module.css';
+
+export default function ChatMessage({ message }) {
+	const { content, isUser, timestamp } = message;
+	const formattedTime = formatEpoch(timestamp);
+
+	return (
+		<div
+			className={`${styles.messageWrapper} ${isUser ? styles.userMessage : styles.aiMessage}`}
+		>
+			{!isUser && (
+				<div className={styles.iconWrapper}>
+					<GhostIcon size={44} />
+				</div>
+			)}
+			<div className={styles.messageContent}>
+				<div className={styles.messageText}>{content}</div>
+				<div className={styles.timestamp}>{formattedTime}</div>
+			</div>
+		</div>
+	);
+}
